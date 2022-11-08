4735c09c8574c6372bbf22f916dcad5bf9d9b1ca
joyqi <magike.net@gmail.com>
2022-11-08 23:04:23 +0800

    add h2v

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # html-to-snabbdom
+
+Transform HTML to Snabbdom VNodes.
+
+## Installation
+
+```bash
+npm install html-to-snabbdom
+```
+
+## Usage
+
+```js
+import h2v from 'html-to-snabbdom';
+
+const vnode = h2v('<div class="foo">Hello World</div>');
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-// TODO: export your library
+import { toVNode, VNode, Fragment } from 'snabbdom';
+
+/**
+ * Create a VNode from a html string
+ */
+ export function h2v(html: string): VNode {
+    const el = document.createElement('div');
+    el.innerHTML = html.trim();
+    const vnode = toVNode(el);
+
+    if (vnode.children && vnode.children.length === 1) {
+        const firstChild = vnode.children[0];
+
+        if (typeof firstChild === 'string') {
+            throw new Error('The html string must have a root element');
+        }
+
+        return firstChild;
+    } else {
+        return Fragment(null, vnode.children);
+    }
+}
